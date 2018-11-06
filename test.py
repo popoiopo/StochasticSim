18e36a509d8d0d00c5df3c7bb0a10473c50ad3f4
LMettrop <lmettrop@gmail.com>
2018-11-06 23:18:36 +0100

    Poging tot ortogonaal

--- test.py
+++ test.py
@@ -6,58 +6,52 @@ import random
 from mpl_toolkits.mplot3d import Axes3D
 
 
-@jit
-def mandelbrot(z, maxiter, horizon, log_horizon):
-    c = z
-    for n in range(maxiter):
-        az = abs(z)
-        if az > horizon:
-            return n - np.log(np.log(az)) / np.log(2) + log_horizon
-        z = z * z + c
-    return 0
+def diagonal2(n):
+    grid = [0 for j in range(n)]
+    for i in range(n):
+        grid[i] = i
+    return grid
 
-@jit
-def checkInMS(z, maxiter, horizon, log_horizon):
-    #     print(z)
-    if mandelbrot(z, maxiter, horizon, log_horizon) == 0:
-        return 1.0
-    return 0.0
+def shuffle2(grid, n, k, block_size, choices):
+    plusmin = [0, 1, 2]
+
+    return grid
+
+def rotate(l, n):
+    return l[n:] + l[:n]
+
+def check_blocks2(n, grid):
+    block_size = int(np.sqrt(n))
+    for j in range(0, n - block_size, block_size):
+        k_values = [c for c in range(block_size)]
+        random.shuffle(k_values)
+        a = int(j / block_size)
+        for c in range(block_size):
+            if grid[k_values[c]+ j] >= j:
+                found = False
+                while not found:
+                    to = a * block_size + k_values[c]
+                    if (grid[to] > j + block_size) or (grid[to] == to):
+                        found = True
+                    else:
+                        k_values = rotate(k_values, -1)
+                temp = grid[to]
+                grid[to] = grid[k_values[c] + j]
+                grid[k_values[c] + j] = temp
+                a += 1
+    return grid
 
 
-def MonteCarlo(s, i, xmin, xmax, ymin, ymax):
-    horizon = 2.0**40
-    log_horizon = np.log(np.log(horizon)) / np.log(2)
-    area = (xmax - xmin) * (ymax - ymin)
-    ctr = 0
-    for j in range(s):
-        x = xmin + (xmax - xmin) * random.random()
-        y = ymin + (ymax - ymin) * random.random()
-        z = x + 1j * y
-        ctr += checkInMS(z, i, horizon, log_horizon)
-    return (ctr / s) * area
 
-
-def plotConvergence(steps, maxIterations, maxSamplings):
-    x = np.linspace(1, maxSamplings, steps + 1).astype('int')
-    y = np.linspace(1, maxIterations, steps + 1).astype('int')
-    results = [[0 for j in range(steps+1)] for i in range(steps+1)]
-    X, Y = np.meshgrid(x, y)
-    s_in = 0
-    i_in = 0
-    for s in x:
-        for i in y:
-            result = MonteCarlo(s, i, -2.0, 0.5, -1.25, 1.25)
-#             print(result)
-            results[s_in][i_in] = result
-            i_in += 1
-        s_in += 1
-        i_in = 0
-    results=np.array([np.array(xi) for xi in results])
+def orthogonal2(n):
+    grid = diagonal2(n)
+    grid = check_blocks2(n, grid)
+    x = np.linspace(0, n-1, n)
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.invert_xaxis()
-    res = ax.plot_surface(X, Y, results)
-    ax.show()
+    ax = fig.add_subplot(111)
+    ax.scatter(x, grid, color='k')
+    ax.hlines([3.5, 7.5, 11.5], 0, 16)
+    ax.vlines([3.5, 7.5, 11.5], 0, 16)
+#     ax.show()
 
-plotConvergence(10, 1000, 800)
-
+orthogonal2(16)

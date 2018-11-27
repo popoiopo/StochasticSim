fc8c88ed796921f43c465cd7c3b8dcb01b14f63d
LMettrop <lmettrop@gmail.com>
2018-11-27 22:41:18 +0100

    parameters naar rates geconvergeert + meet in batches in dezelfde run  ipv meerdere runs met een meeting per run

--- Lennart/Opd 2/FunctionsInPython.py
+++ Lennart/Opd 2/FunctionsInPython.py
@@ -1,6 +1,8 @@
 def averageQueueTimesTwee(lamb, mu):
-    up = 2 * (lamb / mu) ** 3
-    low = lamb * (1 - (lamb / mu) ** 2)
+    rho = lamb/mu
+    print(rho)
+    up = 2 * rho ** 3
+    low = lamb * (1 - rho ** 2)
     return up / low
 
 def averageTimesTwee(lamb, mu):
@@ -20,11 +22,17 @@ def averageQueueCustomersTwee(lamb, mu):
     low = 1 - rho ** 2
     return up / low
 
+l = 8.0/5
+u = 2.0
+print(u)
+
+
+
 print("For 2 servers using variant of M/M/2")
-print(f"Average time in queue calculated = {averageQueueTimesTwee(12, 13)}")
-print(f"Average time calculated = {averageTimesTwee(12, 13)}")
-print(f"Average customers in queue calculated = {averageQueueCustomersTwee(12, 13)}")
-print(f"Average customers calculated = {averageCustomersTwee(12, 13)}\n")
+print(f"Average time in queue calculated = {averageQueueTimesTwee(l, u)}\n")
+# print(f"Average time calculated = {averageTimesTwee(l, u)}")
+# print(f"Average customers in queue calculated = {averageQueueCustomersTwee(l, u)}")
+# print(f"Average customers calculated = {averageCustomersTwee(l, u)}\n")
 
 
 
@@ -35,4 +43,4 @@ def averageQueueTimesOne(lamb, mu):
     return up / low
 
 print("For 1 servers")
-print(f"Average time in queue calculated = {averageQueueTimesOne(12, 13)}")
+print(f"Average time in queue calculated = {averageQueueTimesOne(l, u)}")

cde79c8fbf01caa11702a7ac44b81c11f41808c7
LMettrop <lmettrop@gmail.com>
2018-11-29 20:19:25 +0100

    Long tail parameters gefit, nog meer dingetjes

--- Lennart/Opd 2/FunctionsInPython.py
+++ Lennart/Opd 2/FunctionsInPython.py
@@ -1,8 +1,9 @@
+import math
+
 def averageQueueTimesTwee(lamb, mu):
     rho = lamb/mu
-    print(rho)
-    up = 2 * rho ** 3
-    low = lamb * (1 - rho ** 2)
+    up = (rho ** 3)
+    low = lamb * (1 - (rho ** 2))
     return up / low
 
 def averageTimesTwee(lamb, mu):
@@ -22,10 +23,34 @@ def averageQueueCustomersTwee(lamb, mu):
     low = 1 - rho ** 2
     return up / low
 
+
+
+def C(rho, n):
+    up = ((rho ** n) / math.factorial(n)) * (n / (n - rho))
+    low1 = 0
+    for k in range(n):
+        low1 += ((rho ** k) / math.factorial(k))
+    low2 = ((rho ** n) * n) / (math.factorial(n) * (n - rho))
+    low = low1 + low2
+    return up / low
+
+def ErlangC(A, N):
+    L = (A**N / math.factorial(N)) * (N / (N - A))
+    sum_ = 0
+    for i in range(N):
+        sum_ += (A**i) / math.factorial(i)
+    return (L / (sum_ + L))
+
+def Wq(rho, mu, n):
+    Pq = C(rho, n)
+    a = 1 / (mu * (n - rho))
+    return a * Pq
+
 l = 8.0/5
 u = 2.0
-print(u)
+rho = l/u
 
+print(C(rho, 1) == rho)
 
 
 print("For 2 servers using variant of M/M/2")
@@ -38,9 +63,36 @@ print(f"Average time in queue calculated = {averageQueueTimesTwee(l, u)}\n")
 
 def averageQueueTimesOne(lamb, mu):
     rho = lamb / mu
-    up = rho ** 2
-    low = lamb * (1 - rho)
+    up = rho
+    low = mu * (1 - rho)
     return up / low
 
 print("For 1 servers")
 print(f"Average time in queue calculated = {averageQueueTimesOne(l, u)}")
+
+n=1
+l = 9.5/5
+u = 2.0
+rho = l / u
+print("\nFor n=1")
+print(f"C(rho, 2) = {C(rho, n)}")
+print(f"Wq = {Wq(rho, u, n)}")
+print(f"Wq variant = {Wq(n * rho, u, n)}")
+
+n=2
+print("\nFor n=2")
+print(f"C(rho, 2) = {C(rho, n)}")
+print(f"Wq = {Wq(rho, u, n)}")
+print(f"Wq variant = {Wq(n * rho, u, n)}")
+
+n=3
+print("\nFor n=3")
+print(f"C(rho, 2) = {C(rho, n)}")
+print(f"Wq = {Wq(rho, u, n)}")
+print(f"Wq variant = {Wq(n * rho, u, n)}")
+
+n=4
+print("\nFor n=4")
+print(f"C(rho, 2) = {C(rho, n)}")
+print(f"Wq = {Wq(rho, u, n)}")
+print(f"Wq variant = {Wq(n * rho, u, n)}")
